f1545bb1e4355b9b80103f2f2f2c0c8834f8e897
Igor Kalnitsky <igor@kalnitsky.org>
2013-12-15 22:39:48 +0200

    Change comment format.

--- wp-highlight.js/wp_highlight.js.php
+++ wp-highlight.js/wp_highlight.js.php
@@ -3,7 +3,7 @@
 Plugin Name: wp-highlight.js
 Plugin URI: http://kalnitsky.org/projects/wp-highlight.js/
 Description: This is simple wordpress plugin for <a href="http://softwaremaniacs.org/soft/highlight/en/">highlight.js</a> library. Highlight.js highlights syntax in code examples on blogs, forums and in fact on any web pages. It&acute;s very easy to use because it works automatically: finds blocks of code, detects a language, highlights it.
-Version: 0.3.2
+Version: 0.3.3
 Author: Igor Kalnitsky
 Author URI: http://kalnitsky.org/
 License: 3-clause BSD
@@ -15,11 +15,10 @@ $PLUGIN_DIR =  plugins_url() . '/' . dirname(plugin_basename(__FILE__));
 
 init_hljs_textdomain();  # initialize localization functions: _e(), __()
 
-#
-# Plugin Installation:
-#   : create configuration keys
-#
-
+/**
+ * Plugin Installation:
+ *   - create configuration keys
+ */
 function hljs_install() {
     add_option('hljs_style', 'default.css');
     add_option('hljs_tab_replace', '    ');
@@ -27,11 +26,11 @@ function hljs_install() {
 }
 register_activation_hook(__FILE__, 'hljs_install');
 
-# 
-# Plugin Deinstallation
-#   : delete configuration keys
-#
 
+/**
+ * Plugin Deinstallation
+ *   - delete configuration keys
+ */
 function hljs_deinstall() {
     delete_option('hljs_style');
     delete_option('hljs_tab_replace');
@@ -39,11 +38,12 @@ function hljs_deinstall() {
 }
 register_deactivation_hook(__FILE__, 'hljs_deinstall');
 
-# 
-# Including Script into Page
-#   : include js and css
-#
 
+/**
+ * Attach Highlight.js to the current page
+ *   - attach highlight.pack.js
+ *   - attach colorscheme stylesheet
+ */
 function hljs_include() {
     global $PLUGIN_DIR;
 ?>
@@ -55,20 +55,20 @@ function hljs_include() {
 }
 add_action('wp_head', 'hljs_include');
 
-# 
-# Initialize Localization Functions
-#
 
+/**
+ * Initialize Localization Functions
+ */
 function init_hljs_textdomain() {
     if (function_exists('load_plugin_textdomain')) {
-        load_plugin_textdomain( 'hljs', false, dirname( plugin_basename( __FILE__ ) ) . '/' . 'translations' );
+        load_plugin_textdomain('hljs', false, dirname(plugin_basename( __FILE__ )) . '/' . 'translations');
     }
 }
 
-# 
-# Print Combobox With Styles
-#
 
+/**
+ * Print Combobox With Styles
+ */
 function hljs_get_style_list($currentStyle) {
     $styleDir = '..' . '/' . PLUGINDIR . '/' . dirname(plugin_basename(__FILE__)) . '/' . 'styles'; # dirty hack
 
@@ -88,33 +88,33 @@ function hljs_get_style_list($currentStyle) {
     closedir($dir);
 }
 
-# 
-# Add Settings Page to Admin Menu
-#
-
+/**
+ * Add Settings Page to Admin Menu
+ */
 function hljs_admin_page() {
     if (function_exists('add_submenu_page'))
         add_options_page(__('wp-highlight.js settings'), __('wp-highlight.js'), 'manage_options', 'wp-highlight.js', 'hljs_settings_page');
 }
 add_action('admin_menu', 'hljs_admin_page');
 
-# 
-# Add Settings link to plugin page
-#
 
+/**
+ * Add Settings link to plugin page
+ */
 function hljs_add_settings_link($links, $file) {
     if ($file == plugin_basename(__FILE__)) {
-      $links[] = '<a href="options-general.php?page=wp-highlight.js">'.__('Settings').'</a>';
+      $links[] = '<a href="options-general.php?page=wp-highlight.js">' . __('Settings') . '</a>';
     }
     return $links;
 }
-add_filter('plugin_action_links', 'hljs_add_settings_link', 10, 2 );
+add_filter('plugin_action_links', 'hljs_add_settings_link', 10, 2);
 
-# 
-# Add BB-Tag for highlighting
-#   Usage: [CODE lang=C++]...[/CODE]
-#
 
+/**
+ * Add BB-Tag for highlighting.
+ *
+ *   Usage: [CODE lang=C++]...[/CODE]
+ */
 function hljs_code_handler($atts, $content) {
     $language = $atts['lang'];
     return "<pre class=\"hljs\"><code class=\"$language\">" . ltrim($content, '\n') . '</code></pre>';
@@ -122,15 +122,13 @@ function hljs_code_handler($atts, $content) {
 add_shortcode('code', 'hljs_code_handler');
 
 
-# 
-# Highlight.js Settings Page
-#   : create page
-#
-
+/**
+ * Highlight.js Settings Page
+ */
 function hljs_settings_page() {
     global $PLUGIN_DIR;
 
-    if ( isset( $_POST['cmd'] ) && $_POST['cmd'] == "hljs_save" )
+    if (isset( $_POST['cmd'] ) && $_POST['cmd'] == 'hljs_save')
     {
         update_option('hljs_style', $_POST['hljs_style']);
         update_option('hljs_tab_replace', $_POST['hljs_tab_replace']);
@@ -150,7 +148,7 @@ function hljs_settings_page() {
         <script type="text/javascript" src="<?php echo ($PLUGIN_DIR . '/' . 'highlight.pack.js'); ?>"></script>
         <script type="text/javascript">hljs.initHighlightingOnLoad();</script>
         <link rel="stylesheet" href="<?php echo ($PLUGIN_DIR . '/' . 'styles' . '/default.css'); ?>" />
-      
+
         <style>
             .info { padding: 15px; background: #EDEDED; border: 1px solid #333333; font: 14px #333333 Verdana; margin: 30px 10px 0px 0px; }
 
@@ -169,7 +167,7 @@ function hljs_settings_page() {
 
         <!-- combo box with styles -->
         <p class="section">
-          <label for="hljs_style"><?php echo __('Color Scheme', 'hljs') ?></label><br/>
+          <label for="hljs_style"><?php echo __('Color Scheme:', 'hljs') ?></label><br/>
 
           <select name="hljs_style" id="hljs_style">
              <?php hljs_get_style_list(get_option('hljs_style')); ?>
@@ -178,13 +176,13 @@ function hljs_settings_page() {
 
         <!-- text edit : tab replace -->
         <p class="section">
-          <label for="hljs_tab_replace"><?php echo __('You can replaces TAB (\x09) characters used for indentation in your code with some fixed number of spaces or with a &lt;span&gt; to set them special styling', 'hljs') ?></label><br/>
+          <label for="hljs_tab_replace"><?php echo __('You can replaces TAB (\x09) characters used for indentation in your code with some fixed number of spaces or with a &lt;span&gt; to set them special styling:', 'hljs') ?></label><br/>
           <input type="text" name="hljs_tab_replace" id="hljs_tab_replace" value="<?php echo get_option('hljs_tab_replace') ?>" />
         </p>
 
         <!-- text edit : additional css -->
         <p class="section">
-          <label for="hljs_additional_css"><?php echo __('You can add some additional CSS rules for better display', 'hljs') ?></label><br/>
+          <label for="hljs_additional_css"><?php echo __('You can add some additional CSS rules for better display:', 'hljs') ?></label><br/>
           <textarea type="text" name="hljs_additional_css" id="hljs_additional_css"><?php echo get_option('hljs_additional_css') ?></textarea>
         </p>
 
